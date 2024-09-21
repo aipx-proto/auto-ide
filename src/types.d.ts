4fa8d83cf3febe462cde465f8455f73cffb7e689
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-09-21 11:06:28 -0700

    remove unused typing

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,3 @@
 declare module "https://esm.sh/esbuild-wasm*" {
   export * from "esbuild-wasm";
 }
-
-declare module "https://esm.sh/@azure/identity*" {
-  export * from "@azure/identity";
-}
-
-declare module "https://esm.sh/openai*" {
-  export * from "openai";
-}
